9f5aa73938c0ab726e7c2490a87e888b2544949f
Theodore Bendixson <tedbendixson@me.com>
2018-12-27 16:00:23 -0700

    Removed pragma declarations and imports

--- Day 1/handmade/code/osx_main.mm
+++ Day 1/handmade/code/osx_main.mm
@@ -3,13 +3,8 @@
 //
 // OSX Main
 
-#include <Cocoa/Cocoa.h>
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    
-    #pragma unused(argc)
-    #pragma unused(argv)
-    
     printf("Handmade Hero Finished Running");
 }
