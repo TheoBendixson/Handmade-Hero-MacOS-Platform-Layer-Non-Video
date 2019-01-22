eb4adf3e4fb0e8b504f46c223206844a3bb08e66
Theodore Bendixson <tedbendixson@me.com>
2019-01-21 17:41:25 -0700

    Day 003 Source Code

--- Day 3/handmade/code/osx_main.mm
+++ Day 3/handmade/code/osx_main.mm
@@ -6,10 +6,14 @@
 #include <stdio.h>
 #include <AppKit/AppKit.h>
 
-static float GlobalRenderWidth = 1024;
-static float GlobalRenderHeight = 768;
+#define internal static
+#define local_persist static
+#define global_variable static
 
-static bool Running = true;
+global_variable float globalRenderWidth = 1024;
+global_variable float globalRenderHeight = 768;
+global_variable bool running = true;
+global_variable uint8_t *buffer;
 
 @interface HandmadeMainWindowDelegate: NSObject<NSWindowDelegate>
 @end
@@ -17,7 +21,7 @@ static bool Running = true;
 @implementation HandmadeMainWindowDelegate 
 
 - (void)windowWillClose:(id)sender {
-  Running = false;  
+  running = false;  
 }
 
 @end
@@ -28,10 +32,10 @@ int main(int argc, const char * argv[]) {
 
     NSRect screenRect = [[NSScreen mainScreen] frame];
 
-    NSRect initialFrame = NSMakeRect((screenRect.size.width - GlobalRenderWidth) * 0.5,
-                                     (screenRect.size.height - GlobalRenderHeight) * 0.5,
-                                     GlobalRenderWidth,
-                                     GlobalRenderHeight);
+    NSRect initialFrame = NSMakeRect((screenRect.size.width - globalRenderWidth) * 0.5,
+                                     (screenRect.size.height - globalRenderHeight) * 0.5,
+                                     globalRenderWidth,
+                                     globalRenderHeight);
     
     NSWindow *window = [[NSWindow alloc] initWithContentRect:initialFrame
 					 styleMask: NSWindowStyleMaskTitled |
@@ -46,7 +50,37 @@ int main(int argc, const char * argv[]) {
     [window makeKeyAndOrderFront: nil];
     [window setDelegate: mainWindowDelegate];
 
-    while(Running) {
+    while(running) {
+       
+        if(buffer) {
+            free(buffer);
+        }   
+ 
+        int bitmapWidth = window.contentView.bounds.size.width;
+        int bitmapHeight = window.contentView.bounds.size.height;
+        int bytesPerPixel = 4;
+        size_t pitch = bitmapWidth * bytesPerPixel;
+        buffer = (uint8_t *)malloc(pitch * bitmapHeight);
+
+        @autoreleasepool {
+            NSBitmapImageRep *rep = [[[NSBitmapImageRep alloc]                                                        
+                                        initWithBitmapDataPlanes: &buffer
+                                        pixelsWide: bitmapWidth
+                                        pixelsHigh: bitmapHeight
+                                        bitsPerSample: 8
+                                        samplesPerPixel: 4
+                                        hasAlpha: YES
+                                        isPlanar: NO
+                                        colorSpaceName: NSDeviceRGBColorSpace
+                                        bytesPerRow: pitch
+                                        bitsPerPixel: bytesPerPixel * 8] autorelease]; 
+
+            NSSize imageSize = NSMakeSize(bitmapWidth, bitmapHeight);
+            NSImage *image = [[[NSImage alloc] initWithSize: imageSize] autorelease];
+            [image addRepresentation: rep];
+            window.contentView.layer.contents = image;
+        }
+
         NSEvent* Event;
         
         do {
