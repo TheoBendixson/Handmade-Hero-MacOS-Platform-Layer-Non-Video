d3bea3c3620678c9720def42997bcdaf6872bc86
Theodore Bendixson <tedbendixson@me.com>
2019-01-21 17:25:56 -0700

    Moving end of day three to day 4. Starting day 3 fresh

--- Day 3/handmade/code/osx_main.mm
+++ Day 3/handmade/code/osx_main.mm
@@ -6,102 +6,10 @@
 #include <stdio.h>
 #include <AppKit/AppKit.h>
 
-#define internal static
-#define local_persist static
-#define global_variable static
+static float GlobalRenderWidth = 1024;
+static float GlobalRenderHeight = 768;
 
-typedef int8_t int8;
-typedef int16_t int16;
-typedef int32_t int32;
-typedef int64_t int64;
-
-typedef uint8_t uint8;
-typedef uint16_t uint16;
-typedef uint32_t uint32;
-typedef uint64_t uint64;
-
-global_variable float GlobalRenderWidth = 1024;
-global_variable float GlobalRenderHeight = 768;
-
-global_variable bool running = true;
-global_variable uint8 *buffer;
-global_variable int bitmapWidth;
-global_variable int bitmapHeight;
-global_variable int bytesPerPixel = 4;
-global_variable size_t pitch;
-
-global_variable int offsetX = 0;
-
-void refreshBuffer(NSWindow *window) {
-
-    if(buffer) {
-        free(buffer);
-    }
-
-    bitmapWidth = window.contentView.bounds.size.width;
-    bitmapHeight = window.contentView.bounds.size.height;
-
-    bytesPerPixel = 4;
-    pitch = bitmapWidth * bytesPerPixel;
-    buffer = (uint8 *)malloc(pitch * bitmapHeight);
-
-    @autoreleasepool {
-        NSBitmapImageRep *rep = [[[NSBitmapImageRep alloc] initWithBitmapDataPlanes: &buffer 
-                                  pixelsWide: bitmapWidth
-                                  pixelsHigh: bitmapHeight
-                                  bitsPerSample: 8
-                                  samplesPerPixel: 4
-                                  hasAlpha: YES
-                                  isPlanar: NO
-                                  colorSpaceName: NSDeviceRGBColorSpace
-                                  bytesPerRow: pitch
-                                  bitsPerPixel: bytesPerPixel * 8] autorelease];
-
-        NSSize imageSize = NSMakeSize(bitmapWidth, bitmapHeight);
-        NSImage *image = [[[NSImage alloc] initWithSize: imageSize] autorelease];
-        [image addRepresentation: rep];
-        window.contentView.layer.contents = image;
-    }
-}
-
-void renderWeirdGradient() {
-
-    int width = bitmapWidth;
-    int height = bitmapHeight;
-
-    uint8 *row = (uint8 *)buffer;
-
-    for ( int y = 0; y < height; ++y) {
-
-        uint8 *pixel = (uint8 *)row;
-
-        for(int x = 0; x < width; ++x) {
-            
-            /*
-                Pixel in memory: RR GG BB AA
-            */
-
-            //Red            
-            *pixel = 0; 
-            ++pixel;  
-
-            //Green
-            *pixel = (uint8)y;
-            ++pixel;
-
-            //Blue
-            *pixel = (uint8)x+(uint8)offsetX;
-            ++pixel;
-
-            //Alpha
-            *pixel = 255;
-            ++pixel;          
-        }
-
-        row+= pitch;
-    }
-
-}
+static bool Running = true;
 
 @interface HandmadeMainWindowDelegate: NSObject<NSWindowDelegate>
 @end
@@ -109,13 +17,7 @@ void renderWeirdGradient() {
 @implementation HandmadeMainWindowDelegate 
 
 - (void)windowWillClose:(id)sender {
-    running = false;  
-}
-
-- (void)windowDidResize:(NSNotification *)notification {
-    NSWindow *window = (NSWindow*)notification.object;
-    refreshBuffer(window);
-    renderWeirdGradient();
+  Running = false;  
 }
 
 @end
@@ -132,26 +34,19 @@ int main(int argc, const char * argv[]) {
                                      GlobalRenderHeight);
     
     NSWindow *window = [[NSWindow alloc] initWithContentRect:initialFrame
-                         styleMask: NSWindowStyleMaskTitled |
-                                    NSWindowStyleMaskClosable |
-                                    NSWindowStyleMaskMiniaturizable |
-                                    NSWindowStyleMaskResizable 
-                         backing:NSBackingStoreBuffered
-                         defer:NO];    
+					 styleMask: NSWindowStyleMaskTitled |
+						    NSWindowStyleMaskClosable |
+						    NSWindowStyleMaskMiniaturizable |
+						    NSWindowStyleMaskResizable 
+					 backing:NSBackingStoreBuffered
+					 defer:NO];    
 
-    [window setBackgroundColor: NSColor.blackColor];
+    [window setBackgroundColor: NSColor.redColor];
     [window setTitle: @"Handmade Hero"];
     [window makeKeyAndOrderFront: nil];
     [window setDelegate: mainWindowDelegate];
-    window.contentView.wantsLayer = YES;
-    
-    while(running) {
-    
-        refreshBuffer(window); 
-        renderWeirdGradient();
 
-        offsetX++;
-        
+    while(Running) {
         NSEvent* Event;
         
         do {
---
+++ Day 4/handmade/code/osx_main.mm
@@ -0,0 +1,171 @@
+// Handmade Hero OSX
+// By Ted Bendixson
+//
+// OSX Main
+
+#include <stdio.h>
+#include <AppKit/AppKit.h>
+
+#define internal static
+#define local_persist static
+#define global_variable static
+
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+global_variable float GlobalRenderWidth = 1024;
+global_variable float GlobalRenderHeight = 768;
+
+global_variable bool running = true;
+global_variable uint8 *buffer;
+global_variable int bitmapWidth;
+global_variable int bitmapHeight;
+global_variable int bytesPerPixel = 4;
+global_variable size_t pitch;
+
+global_variable int offsetX = 0;
+
+void refreshBuffer(NSWindow *window) {
+
+    if(buffer) {
+        free(buffer);
+    }
+
+    bitmapWidth = window.contentView.bounds.size.width;
+    bitmapHeight = window.contentView.bounds.size.height;
+
+    bytesPerPixel = 4;
+    pitch = bitmapWidth * bytesPerPixel;
+    buffer = (uint8 *)malloc(pitch * bitmapHeight);
+
+    @autoreleasepool {
+        NSBitmapImageRep *rep = [[[NSBitmapImageRep alloc] initWithBitmapDataPlanes: &buffer 
+                                  pixelsWide: bitmapWidth
+                                  pixelsHigh: bitmapHeight
+                                  bitsPerSample: 8
+                                  samplesPerPixel: 4
+                                  hasAlpha: YES
+                                  isPlanar: NO
+                                  colorSpaceName: NSDeviceRGBColorSpace
+                                  bytesPerRow: pitch
+                                  bitsPerPixel: bytesPerPixel * 8] autorelease];
+
+        NSSize imageSize = NSMakeSize(bitmapWidth, bitmapHeight);
+        NSImage *image = [[[NSImage alloc] initWithSize: imageSize] autorelease];
+        [image addRepresentation: rep];
+        window.contentView.layer.contents = image;
+    }
+}
+
+void renderWeirdGradient() {
+
+    int width = bitmapWidth;
+    int height = bitmapHeight;
+
+    uint8 *row = (uint8 *)buffer;
+
+    for ( int y = 0; y < height; ++y) {
+
+        uint8 *pixel = (uint8 *)row;
+
+        for(int x = 0; x < width; ++x) {
+            
+            /*
+                Pixel in memory: RR GG BB AA
+            */
+
+            //Red            
+            *pixel = 0; 
+            ++pixel;  
+
+            //Green
+            *pixel = (uint8)y;
+            ++pixel;
+
+            //Blue
+            *pixel = (uint8)x+(uint8)offsetX;
+            ++pixel;
+
+            //Alpha
+            *pixel = 255;
+            ++pixel;          
+        }
+
+        row+= pitch;
+    }
+
+}
+
+@interface HandmadeMainWindowDelegate: NSObject<NSWindowDelegate>
+@end
+
+@implementation HandmadeMainWindowDelegate 
+
+- (void)windowWillClose:(id)sender {
+    running = false;  
+}
+
+- (void)windowDidResize:(NSNotification *)notification {
+    NSWindow *window = (NSWindow*)notification.object;
+    refreshBuffer(window);
+    renderWeirdGradient();
+}
+
+@end
+
+int main(int argc, const char * argv[]) {
+
+    HandmadeMainWindowDelegate *mainWindowDelegate = [[HandmadeMainWindowDelegate alloc] init];
+
+    NSRect screenRect = [[NSScreen mainScreen] frame];
+
+    NSRect initialFrame = NSMakeRect((screenRect.size.width - GlobalRenderWidth) * 0.5,
+                                     (screenRect.size.height - GlobalRenderHeight) * 0.5,
+                                     GlobalRenderWidth,
+                                     GlobalRenderHeight);
+    
+    NSWindow *window = [[NSWindow alloc] initWithContentRect:initialFrame
+                         styleMask: NSWindowStyleMaskTitled |
+                                    NSWindowStyleMaskClosable |
+                                    NSWindowStyleMaskMiniaturizable |
+                                    NSWindowStyleMaskResizable 
+                         backing:NSBackingStoreBuffered
+                         defer:NO];    
+
+    [window setBackgroundColor: NSColor.blackColor];
+    [window setTitle: @"Handmade Hero"];
+    [window makeKeyAndOrderFront: nil];
+    [window setDelegate: mainWindowDelegate];
+    window.contentView.wantsLayer = YES;
+    
+    while(running) {
+    
+        refreshBuffer(window); 
+        renderWeirdGradient();
+
+        offsetX++;
+        
+        NSEvent* Event;
+        
+        do {
+            Event = [NSApp nextEventMatchingMask: NSEventMaskAny
+                                       untilDate: nil
+                                          inMode: NSDefaultRunLoopMode
+                                         dequeue: YES];
+            
+            switch ([Event type]) {
+                default:
+                    [NSApp sendEvent: Event];
+            }
+        } while (Event != nil);
+    }
+    
+    printf("Handmade Finished Running");
+}
