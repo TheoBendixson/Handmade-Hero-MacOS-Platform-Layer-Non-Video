6b11c0858b37ea5e514cb72f8b4edaa9f9d77e71
Theodore Bendixson <tedbendixson@me.com>
2019-05-07 06:43:31 -0600

    Using Info.plist from resources directory

--- Day 6/handmade/code/build.sh
+++ Day 6/handmade/code/build.sh
@@ -3,6 +3,7 @@ echo Building Handmade Hero
 OSX_LD_FLAGS="-framework AppKit -framework IOKit"
 
 HANDMADE_CODE_PATH="../handmade/code/"
+HANDMADE_RESOURCES_PATH="../handmade/resources/"
 
 OSX_TARGET_INCLUDES="${HANDMADE_CODE_PATH}osx_main.mm 
                      ${HANDMADE_CODE_PATH}osx_handmade_windows.mm
@@ -14,5 +15,5 @@ clang -g $OSX_LD_FLAGS -o handmade $OSX_TARGET_INCLUDES
 rm -rf handmade.app
 mkdir handmade.app
 cp handmade handmade.app/handmade
-cp ${HANDMADE_CODE_PATH}Info.plist handmade.app/Info.plist
+cp ${HANDMADE_RESOURCES_PATH}Info.plist handmade.app/Info.plist
 popd
