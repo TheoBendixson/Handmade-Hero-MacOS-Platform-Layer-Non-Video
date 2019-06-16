faa34763bc701801967a31e3fb374b818acff001
Theodore Bendixson <tedbendixson@me.com>
2019-06-16 07:48:24 -0600

    Day012 Source

---
+++ Day012/handmade/code/build.sh
@@ -0,0 +1,18 @@
+echo Building Handmade Hero
+
+OSX_LD_FLAGS="-framework AppKit 
+              -framework IOKit
+              -framework AudioToolbox"
+
+HANDMADE_RESOURCES_PATH="../handmade/resources/"
+
+HANDMADE_CODE_PATH="../handmade/cpp/code"
+
+mkdir ../../build
+pushd ../../build
+clang -g $OSX_LD_FLAGS -o handmade ${HANDMADE_CODE_PATH}/"handmade.cpp" "../handmade/code/osx_main.mm" 
+rm -rf handmade.app
+mkdir handmade.app
+cp handmade handmade.app/handmade
+cp ${HANDMADE_RESOURCES_PATH}Info.plist handmade.app/Info.plist
+popd
---
+++ Day012/handmade/code/osx_main.h
@@ -0,0 +1,15 @@
+#include "../cpp/code/handmade.cpp"
+#import <AppKit/AppKit.h>
+
+struct MacOSSoundOutput {
+    int samplesPerSecond; 
+    int bytesPerSample;
+    int toneHz;
+    int wavePeriod;
+    uint32 runningSampleIndex;
+    real32 tSine;
+    uint32 bufferSize;
+    uint32 writeCursor;
+    uint32 playCursor;
+    void *data;
+};
